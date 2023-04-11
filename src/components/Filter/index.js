e5822362a4af65457e146e06c0e8c7f145728031
Yure Rafael <112043267+yure07@users.noreply.github.com>
2023-04-11 13:18:57 -0300

    target_blank

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,19 +1,30 @@
 import './filter.css'
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export default function Filter () {
+
+useEffect(()=>{
+    
+    let link = document.querySelectorAll('a');
+    for (var i=0; i<link.length; i++) {
+        link[i].removeAttribute("target");
+    }
+
+},[])
+
     return(
         <div className='container-filter'>
             <ul>
-                <li> <Link to='/filter/mouses'> Mouses </Link> </li> <hr/>
-                <li> <Link to='/filter/teclados'> Teclados </Link> </li> <hr/>
-                <li> <Link to='/filter/headsets'> Headsets </Link> </li> <hr/>
-                <li> <Link to='/filter/mousepads'> Mousepads </Link> </li> <hr/>
-                <li> <Link to='/filter/gabinetes'> Gabinetes </Link> </li> <hr/>
-                <li> <Link to='/filter/switches'> Switches </Link> </li> <hr/>
-                <li> <Link to='/filter/cabo-teclado'> Cabos para Teclado </Link> </li> <hr/>
-                <li> <Link to='/filter/kits'> Kits </Link> </li> <hr/>
-                <li> <Link to='/filter/soundbar'> SoundBar </Link> </li> <hr/>
+                <li> <Link to='/filter/mouses' target='_blank' id='desgraca'> Mouses </Link> </li> <hr/>
+                <li> <Link to='/filter/teclados' target='_blank'> Teclados </Link> </li> <hr/>
+                <li> <Link to='/filter/headsets' target='_blank'> Headsets </Link> </li> <hr/>
+                <li> <Link to='/filter/mousepads' target='_blank'> Mousepads </Link> </li> <hr/>
+                <li> <Link to='/filter/gabinetes' target='_blank'> Gabinetes </Link> </li> <hr/>
+                <li> <Link to='/filter/switches' target='_blank'> Switches </Link> </li> <hr/>
+                <li> <Link to='/filter/cabo-teclado' target='_blank'> Cabos para Teclado </Link> </li> <hr/>
+                <li> <Link to='/filter/kits' target='_blank'> Kits </Link> </li> <hr/>
+                <li> <Link to='/filter/soundbar' target='_blank'> SoundBar </Link> </li> <hr/>
             </ul>
         </div>
     )
